c16ddcb889943a4500110b8c8d039b5763d265f6
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-01-23 12:52:31 +0100

    mods

--- frontend/server.js
+++ frontend/server.js
@@ -1,12 +1,31 @@
 'use strict';
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = new express();
-//const router = require('router');
+const request = require('request');
+
 const port = 8000;
 
-//app.use(bodyParser.json());
+app.set('view engine', 'pug');
+app.set('views', './views/pages');
+
+app.get('/', (req, res) => {
+  request('http://localhost:4000/domande/0', (err, response, body) => {
+    if (err) return res.render('error', {err: err});
+    
+    res.render('index', {
+      title: 'Home',
+      domande: JSON.parse(body)
+    });
+  });
+});
+
+
+app.all('*', (req, res) => {
+  res.render('404', {
+    title: 'Pagina non trovata'
+  });
+});
 
 app.listen(port, () => {
-  console.log(`backend listening on port ${port}`);
+  console.log(`FRONTEND listening on port ${port}`);
 });
