aa819623107d42d41bc2d4e340fb7ebe3946f291
angelo jian <angelo jian>
2017-02-13 20:15:12 +0100

    ancor navbar and section work in progress

--- frontend/static/scroll.js
+++ frontend/static/scroll.js
@@ -42,15 +42,40 @@ function navbarScroll(elem) {
   scrollZhou(docID, 150);
 }
 //todo se ri riesce a usarlo si puo activare i link della navbar
-function isInViewport(element) {
+var userHasScrolled = false;
+var ArraySection =[];
+var ArraySectionLength;
+window.onload = getNavbarAncor();
+function getNavbarAncor(){
+  var navbarLinks = document.getElementsByClassName('static-navbar-link');
+  var navbarLinksLength = navbarLinks.length;
+  for (var navbarLinksIndex = 0; navbarLinksIndex < navbarLinksLength; navbarLinksIndex++){
+    ArraySection.push(navbarLinks[navbarLinksIndex].hash.substr(1));
+  }
+  ArraySectionLength = ArraySection.length;
+  console.log(ArraySection, ArraySectionLength);
+}
+window.onscroll = function(e){
+  userHasScrolled =true;
+  if(userHasScrolled){
+    for (var navbarLinksIndex = 0; navbarLinksIndex < ArraySectionLength; navbarLinksIndex++){
+      if(isInViewport(ArraySection[navbarLinksIndex])){
+        
+      }
+    }
+    userHasScrolled = false;
+  }
+}
+function isInViewport(element) {//return true if is in viewport
   element = document.getElementById(element);
   var rect = element.getBoundingClientRect();
   var html = document.documentElement;
   return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
+    rect.top <= 0 &&
+    rect.bottom >= 0 /*&&
+    rect.left >= 0  &&
     rect.bottom <= (window.innerHeight || html.clientHeight) &&
-    rect.right <= (window.innerWidth || html.clientWidth)
+  rect.right <= (window.innerWidth || html.clientWidth)*/
   );
 }
 
