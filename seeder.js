0cb977d27d1f487f7877652ba75c2a4809e5ff7d
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-02-20 12:54:12 +0100

    mods

--- seeder.js
+++ seeder.js
@@ -14,15 +14,22 @@ const fns = [
         fns.push((cb) => {
           request(`http://localhost:4000/docenti/${classe.id}`, (err, results, res) => {
             if (err) cb(err);
-            cb(null, JSON.parse(results.body));
+            cb(null, {
+              [classe.id]: JSON.parse(results.body)
+            });
           })
         });
       });
 
       async.parallel(fns, (err, results)=>{
         if (err) cb(err);
-
-        cb(null, results);
+        const out = {};
+        results.forEach(e => {
+          const key = Object.keys(e)[0];
+          out[key] = e[key];
+        });
+        
+        cb(null, out);
       })
     });
   },
@@ -49,6 +56,28 @@ async.parallel(fns, (err, results) => {
   const domandeDocenti = results[1];
   const domandeGenerali = results[2];
   
+  for (let studentIndex = 0; studentIndex < 18; studentIndex++) {
+    classi.forEach((classe) => {
+      const out = {
+        docenti: []
+      };
+      
+      docenti[classe.id].forEach(docente => {
+        const dataDocente = {
+          id: docente.id,
+          domande: []
+        };
+        out.docenti.push(dataDocente);
+        domandeDocenti.forEach(domanda => {
+          dataDocente.domande.push({
+            id: domanda.id,
+            voto: 5
+          })
+        })
+      });
+      
+    });
+  }
   //todo da qui dobbiamo comporre tutte le possibili risposte ai questionari e poi inviarle
 });
 
