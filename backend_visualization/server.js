d4c5380cac1baa640eaa0af2617cd9f4f7c1fe96
zhou jian <zhou_jian@MacBook-Pro-di-zhou.local>
2017-04-30 16:12:40 +0200

    re-work API

--- backend_visualization/server.js
+++ backend_visualization/server.js
@@ -4,10 +4,9 @@ const explorer = require('express-explorer');
 const mysql = require('mysql');
 const config = require('./config.json');
 const parallel = require('async').parallel;
-const materie_scientifiche = require('./tipoMaterie.json').T_Scientifico;
-const materie_letteratura = require('./tipoMaterie.json').T_Letteratura;
-const materie_lingue = require('./tipoMaterie.json').T_Lingue;
-const materie_altro = require('./tipoMaterie.json').T_Altro;
+const votazione_generale = require('./endpoints/votazione_generale.js');
+const votazioni_docenti = require('./endpoints/votazione_docenti.js');
+const materie = require('./tipoMaterie.json');
 const pool = mysql.createPool({
   connectionLimit: 10,
   host: "rds.soluzionifutura.it",
@@ -43,10 +42,10 @@ const exposeList = (tableName, sorter) => {
         rows.forEach(docente => {
           let type;
           const docente_materia = docente.materia;
-          if (isInArray(docente_materia, materie_scientifiche)) type = "Materia Scientifica"
-          if (isInArray(docente_materia, materie_letteratura)) type = "Letteratura"
-          if (isInArray(docente_materia, materie_lingue)) type = "Lingua"
-          if (isInArray(docente_materia, materie_altro)) type = "Altro"
+          if (isInArray(docente_materia, materie.T_Scientifico)) type = "Materia Scientifica"
+          if (isInArray(docente_materia, materie.T_Letteratura)) type = "Letteratura"
+          if (isInArray(docente_materia, materie.T_Lingue)) type = "Lingua"
+          if (isInArray(docente_materia, materie.T_Altro)) type = "Altro"
           const obj_docente = {
             id: docente.id,
             nome: docente.nome,
@@ -67,229 +66,8 @@ const exposeList = (tableName, sorter) => {
 exposeList('domande', 'ordine');
 exposeList('docenti', 'cognome');
 
-//GET STATISTICHE GENERALI DELLA SCUOLA
-app.get('/votazioni/scuola', (req, res) => {
-  pool.query(`SELECT idDomanda , voto ,COUNT(*) as countValue FROM votazioni GROUP BY idDomanda , voto ORDER BY idDomanda , voto ASC`, (err, rows, fields) => {
-    if (err) return res.status(705).json(err);
-    let countVal = [];
-    let index, countTotRows, sommaAvgRows, countAvgRows = 0;
-    rows.forEach(countRows => {
-      //MI SALVO I DATI DELL'OGGETTO PRESO IN ESAME
-      const idDomandaRows = countRows.idDomanda;
-      const votoRows = countRows.voto;
-      const countValueRows = countRows.countValue;
-      //CONTROLLO IF...
-      if (index === idDomandaRows) { //VUOL DIRE CHE SIAMO ANCORA NELLA STESSA DOMANDA
-        //AGGIORNAMENTO VALORI COME CONTEGGIO E SOMMA
-        const n_countVal = countVal.length - 1;
-        if (votoRows !== -1) { //SE IL VOTO NON E' NULLO
-          countAvgRows += countValueRows;
-          sommaAvgRows += countValueRows * votoRows;
-        }
-        countTotRows += countValueRows;
-        const votazioneRows = {
-          value: votoRows,
-          count: countValueRows
-        };
-        countVal[n_countVal].countVal.push(votazioneRows);
-        countVal[n_countVal].countTot = countTotRows;
-        countVal[n_countVal].avg = sommaAvgRows / countAvgRows;
-      } else { //SE L'ID DELLA DOMANDA NON COINCIDE CON L'ID DOMANDA DI PRIMA ...
-        //RESET VALORI PERCHE' ABBIAMO CAMBIATO DOMANDA
-        countTotRows = countValueRows;
-        countAvgRows = sommaAvgRows = 0;
-        if (votoRows !== -1) { //SE IL VOTO NON E' NULLO
-          countAvgRows = countTotRows;
-          sommaAvgRows = countAvgRows * votoRows;
-        }
-        const votazioneRows = { //STRUTTURA VOTAZIONE DOMANDA
-          value: votoRows,
-          count: countTotRows
-        };
-        let domanda = { // STRUTTURA DOMANDA
-          idDomanda: idDomandaRows,
-          countTot: countTotRows,
-          avg: sommaAvgRows / countAvgRows,
-          countVal: [votazioneRows]
-        };
-        index = idDomandaRows; //SET DEL NUOVO ID DOMANDA
-        countVal.push(domanda); //PUSH NUOVA DOMANDA NELL'ARRAY DOMANDE
-      }
-    });
-    res.json(countVal);
-  });
-})
-
-app.get('/votazioni/docenti', (req, res) => {
-  const idDocenteQS = req.query.idDocente;
-  const queryToSend = [
-    (cb) => {
-      let whereString = '';
-      if (idDocenteQS !== undefined && idDocenteQS !== '') {
-        whereString = `WHERE idDocente = ${idDocenteQS}`;
-      }
-      if (idDocenteQS === 'null') {
-        whereString = `WHERE idDocente IS NULL`;
-      }
-      pool.query(`SELECT idDocente , voto , idDomanda ,COUNT(*) as countValue FROM votazioni ${whereString} GROUP BY voto , idDomanda , idDocente ORDER BY idDocente , idDomanda , voto ASC`, (err, rows, fields) => {
-        if (err) return cb(err);
-        let statisticheDocenti = [];
-        let indexDomanda, indexDocente, sommaAvgTot, countAvgTot, sommaAvgDomanda, countAvgDomanda, countTotDomanda = 0;
-        rows.forEach(data => {
-          const idDocenteData = data.idDocente;
-          const idDomandaData = data.idDomanda;
-          const voto = data.voto;
-          const countDomanda = data.countValue;
-          if (indexDocente === idDocenteData && indexDomanda === idDomandaData) {
-            //stesso docente e domanda
-            if (voto !== -1) {
-              countAvgTot += countDomanda;
-              sommaAvgTot += countDomanda * voto;
-              countAvgDomanda += countDomanda;
-              sommaAvgDomanda += countDomanda * voto;
-            };
-            countTotDomanda += countDomanda;
-            const countValueInProgress = {
-              value: voto,
-              count: countDomanda
-            }
-            const n_statisticheDocenti = statisticheDocenti.length - 1;
-            const n_valutazione = statisticheDocenti[n_statisticheDocenti].valutazione.length - 1;
-            statisticheDocenti[n_statisticheDocenti].avgTot = sommaAvgTot / countAvgTot;
-            statisticheDocenti[n_statisticheDocenti].valutazione[n_valutazione].countVal.push(countValueInProgress);
-            statisticheDocenti[n_statisticheDocenti].valutazione[n_valutazione].countTot = countTotDomanda;
-            statisticheDocenti[n_statisticheDocenti].valutazione[n_valutazione].avg = sommaAvgDomanda / countAvgDomanda;
-          } else if (indexDocente === idDocenteData && indexDomanda !== idDomandaData) {
-            //stesso docente ma domanda cambiata
-            indexDomanda = idDomandaData;
-            countAvgDomanda = sommaAvgDomanda = 0;
-            if (voto !== -1) {
-              countAvgTot += countDomanda;
-              sommaAvgTot += countDomanda * voto;
-              countAvgDomanda = countDomanda;
-              sommaAvgDomanda = countDomanda * voto;
-            };
-            countTotDomanda = countDomanda;
-            const countValueInProgress = {
-              value: voto,
-              count: countDomanda
-            }
-            let valutazioneInProgress = {
-              idDomanda: idDomandaData,
-              countTot: countDomanda,
-              avg: sommaAvgDomanda / countAvgDomanda,
-              countVal: [countValueInProgress]
-            }
-            const n_statisticheDocenti = statisticheDocenti.length - 1;
-            statisticheDocenti[n_statisticheDocenti].avgTot = sommaAvgTot / countAvgTot;
-            statisticheDocenti[n_statisticheDocenti].valutazione.push(valutazioneInProgress);
-          } else {
-            //cambio docente 
-            indexDocente = idDocenteData;
-            indexDomanda = idDomandaData;
-            countAvgTot = sommaAvgTot = countAvgDomanda = sommaAvgDomanda = 0;
-            if (voto !== -1) {
-              countAvgTot = countDomanda;
-              sommaAvgTot = countDomanda * voto;
-              countAvgDomanda = countDomanda;
-              sommaAvgDomanda = countDomanda * voto;
-            };
-            countTotDomanda = countDomanda;
-            const countValueInProgress = {
-              value: voto,
-              count: countDomanda
-            }
-            let valutazioneInProgress = {
-              idDomanda: idDomandaData,
-              countTot: countDomanda,
-              avg: sommaAvgDomanda / countAvgDomanda,
-              countVal: [countValueInProgress]
-            }
-            let docenteInProgress = {
-              idDocente: idDocenteData,
-              avgTot: sommaAvgTot / countAvgTot,
-              valutazione: [valutazioneInProgress]
-            }
-            statisticheDocenti.push(docenteInProgress);
-          }
-        })
-        cb(null, statisticheDocenti);
-      })
-    },
-    (cb) => {
-      let whereString = '';
-      if (idDocenteQS !== undefined && idDocenteQS !== '') {
-        whereString = `WHERE id = ${idDocenteQS}`;
-      };
-      let generale = {
-        idDocente: null,
-        nome: null,
-        cognome: null,
-        materia: null,
-        tipo_materia: null,
-        avgTot: 0,
-        valutazione: []
-      };
-      const arrayDocenti = [];
-      if (idDocenteQS === 'null') { //AGGIUNTA DOCENTE GENERALE
-        arrayDocenti.push(generale);
-        cb(null, arrayDocenti);
-      } else {
-        pool.query(`SELECT * FROM docenti ${whereString} ORDER BY id ASC`, (err, rows, fields) => {
-          if (err) return cb(err); //  ERRORE GET DATI DEI DOCENTI
-          if (idDocenteQS === undefined || idDocenteQS === '') { //AGGIUNTA DOCENTE GENERALE
-            arrayDocenti.push(generale);
-          }
-          rows.forEach(docente => {
-            let type;
-            const docente_materia = docente.materia;
-            if (isInArray(docente_materia, materie_scientifiche)) type = "Materia Scientifica"
-            if (isInArray(docente_materia, materie_letteratura)) type = "Letteratura"
-            if (isInArray(docente_materia, materie_lingue)) type = "Lingua"
-            if (isInArray(docente_materia, materie_altro)) type = "Altro"
-            let docenteRows = {
-              idDocente: docente.id,
-              nome: docente.nome,
-              cognome: docente.cognome,
-              materia: docente_materia,
-              tipo_materia: type,
-              avgTot: 0,
-              valutazione: []
-            };
-            arrayDocenti.push(docenteRows);
-          })
-          cb(null, arrayDocenti);
-        })
-      }
-    }
-  ];
-  parallel(queryToSend, (err, results) => {
-    if (err) {
-      res.status(404).json({
-        error: {
-          status: 404,
-          statusCode: 404,
-          message: err
-        }
-      });
-    } else {
-      let docentiArray = results[1];
-      const valutazioneDocenti = results[0];
-      const n_docentiArray = docentiArray.length;
-      const n_valutazioneDocenti = valutazioneDocenti.length;
-      for (let i = 0; i < n_docentiArray; i++) {
-        for (let y = 0; y < n_valutazioneDocenti; y++) {
-          if (docentiArray[i].idDocente === valutazioneDocenti[y].idDocente) {
-            docentiArray[i].avgTot = valutazioneDocenti[y].avgTot;
-            docentiArray[i].valutazione = valutazioneDocenti[y].valutazione;
-            break;
-          }
-        }
-      }
-      res.json(docentiArray);
-    }
-  });
-})
+app.get('/votazioni/scuola',votazione_generale);//GET VOTAZIONE GENERALE SCUOLA
+app.get('/votazioni/docenti',votazioni_docenti);//GET VOTAZIONE GENERALE PER I DOCENTI
 
 // GET DOMANDE IN BASE AL TIPO
 app.get(`/domande/:type`, (req, res) => {
